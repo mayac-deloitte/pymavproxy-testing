96f7753523bb4b644c36074e0b5ff03579c013bd
mayac-deloitte <mcohen7@deloitte.com.au>
2024-08-15 14:52:59 +1000

    env variables

--- network.py
+++ network.py
@@ -1,6 +1,11 @@
-TCP_IP_ADDRESS = '192.168.188.31'
+# Can change to env. or config.yaml when neccessary
+
+# Networking
+TCP_IP_ADDRESS = '192.168.1.57'
 TCP_PORT = 5763 
 DEFAULT_CONNECTION_STRING = f'tcp:{TCP_IP_ADDRESS}:{TCP_PORT}'
 
+# Default settings
 ARM_COMMAND = 1
 DISARM_COMMAND = 0
+DEFFAULT_ALTITUDE = 10
